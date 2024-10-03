9b20e2c3dfc934ec908bdafab1397677a8a7a783
Asiria05 <mapachesfuriosos0@gmail.com>
2024-10-03 17:47:15 -0600

    x

---
+++ petcareAPI/src/test/java/mx/petcare/mascotas/petcareAPI/PetcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.petcare.mascotas.petcareAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
